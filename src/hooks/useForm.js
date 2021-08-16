9bccd55435293a58bc14464ad055603e48db5d19
Christopher Greene <greenebasecamp@gmail.com>
2021-08-15 20:38:11 -0400

    Major progress

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,26 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+const useForm = (initialValues) => {
+    //representing state as AN OBJECT - ref guided project for format if needed
+    const [values, setValues] = useState('formValues', initialValues);
+    
+    const handleChanges = e => {
+        console.log("User typed an input:", e.target.name);
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setValues("");
+    };
+
+    return [values, handleChanges, handleSubmit];
+}
+
+
+
+export default useForm;
